7886ac407b99d30b84b395a7e8acec05ac9c246a
Melanocetus <98456147+Melanocetuss@users.noreply.github.com>
2025-06-05 07:11:08 +0300

    update_complete MediatR

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/Mediator/Commands/OrderingCommands/RemoveOrderingCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.Mediator.Commands.OrderingCommands
+{
+    public class RemoveOrderingCommand : IRequest
+    {
+        public int OrderingID { get; set; }
+        public RemoveOrderingCommand(int orderingID)
+        {
+            OrderingID = orderingID;
+        }
+    }
+}
---
+++ Services/Order/Presentation/MultiShop,Order.WebApi/Controllers/OrderingsController.cs
@@ -0,0 +1,54 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Order.Application.Features.Mediator.Commands.OrderingCommands;
+using MultiShop.Order.Application.Features.Mediator.Queries.OrderingQueries;
+
+namespace MultiShop_Order.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderingsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public OrderingsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> OrderingList()
+        {
+            var values = await _mediator.Send(new GetOrderingQuery());
+            return Ok(values);
+        }
+
+        [HttpGet("GetOrderingById")]
+        public async Task<IActionResult> GetOrderingById(int orderingID)
+        {
+            var values = await _mediator.Send(new GetOrderingByIdQuery(orderingID));
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateOrdering(CreateOrderingCommand command) 
+        {
+            await _mediator.Send(command);
+            return Ok("Sipariş Ekleme İşlemi Başarıyla Gerçekleşti.");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteOrdering(int orderingID)
+        {
+            await _mediator.Send(new RemoveOrderingCommand(orderingID));
+            return Ok("Sipariş Silme İşlemi Başarıyla Gerçekleşti.");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateOrdering(UpdateOrderingCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Sipariş Güncelleme İşlemi Başarıyla Gerçekleşti.");
+        }
+    }
+}
