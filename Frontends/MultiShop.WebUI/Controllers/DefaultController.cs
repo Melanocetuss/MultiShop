13fdaeab91565c886af38d5fdbd4f6aa58391d93
Melanocetus <98456147+Melanocetuss@users.noreply.github.com>
2025-06-10 19:54:15 +0300

    update_default-UILayout-Complete

---
+++ Frontends/MultiShop.WebUI/Controllers/DefaultController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.FirstBreadcrump = "Ana Sayfa";
+            ViewBag.SecondBreadcrump = "Genel Bakış";
+            return View();
+        }
+    }
+}
