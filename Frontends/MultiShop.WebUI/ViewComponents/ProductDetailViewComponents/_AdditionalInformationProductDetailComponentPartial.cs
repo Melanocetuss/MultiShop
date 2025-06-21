e7bea18d5088a508d66b91224b72d717263e20e6
Melanocetus <98456147+Melanocetuss@users.noreply.github.com>
2025-06-21 17:21:30 +0300

    update_ProductDetail-AdminUI-UserUI-Complete

--- Frontends/MultiShop.WebUI/ViewComponents/ProductDetailViewComponents/_AdditionalInformationProductDetailComponentPartial.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/ProductDetailViewComponents/_AdditionalInformationProductDetailComponentPartial.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.ProductDetailDtos;
+using Newtonsoft.Json;
+using System.Net.Http;
 
 namespace MultiShop.WebUI.ViewComponents.ProductDetailViewComponents
 {
     public class _AdditionalInformationProductDetailComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _AdditionalInformationProductDetailComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string ProductID)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7127/api/ProductDetails/GetProductDetailByProductId?ProductID={ProductID}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<GetByIdProductDetailDto>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
--- Frontends/MultiShop.WebUI/ViewComponents/ProductDetailViewComponents/_DescriptionProductDetailComponentPartial.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/ProductDetailViewComponents/_DescriptionProductDetailComponentPartial.cs
@@ -1,11 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.ProductDetailDtos;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.ViewComponents.ProductDetailViewComponents
 {
     public class _DescriptionProductDetailComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _DescriptionProductDetailComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string ProductID)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7127/api/ProductDetails/GetProductDetailByProductId?ProductID={ProductID}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<GetByIdProductDetailDto>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
