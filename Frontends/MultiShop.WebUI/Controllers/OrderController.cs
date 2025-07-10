984b387d09fe82fa4d7fe94431f96bf232631856
Melanocetus <98456147+Melanocetuss@users.noreply.github.com>
2025-07-10 06:57:07 +0300

    Update_Order_Frontend

---
+++ Frontends/MultiShop.WebUI/Controllers/OrderController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.FirstBreadcrump = "Ana Sayfa";
+            ViewBag.SecondBreadcrump = "Sipariş Ekranı";
+
+            return View();
+        }
+    }
+}
